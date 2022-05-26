99d40926449e1f71399d97b31941871001dbd2e6
noahtq <vfx.noaht@gmail.com>
2022-05-26 11:36:39 -0500

    Add overlay info boxes

---
+++ javascript/tech.js
@@ -0,0 +1,33 @@
+
+const icons = document.querySelectorAll('#tech .icon img');
+const overlays = document.querySelectorAll('.info-overlay');
+
+function iconOpacityChange(event) {
+    //lower non-target icons opacities
+    icons.forEach(element => {
+        element.style.opacity = '0.25';
+    })
+    event.target.style.opacity = '1';
+    
+    //turn on overlay
+    overlayNode = event.target.parentElement.children[0];
+    overlayNode.style.opacity = '1';
+}
+
+function iconOpacityDefault(event) {
+    //set icon opacities back to default
+    icons.forEach(element => {
+        element.style.opacity = '';
+    })
+    
+    //turn off overlay
+    overlayNode = event.target.parentElement.children[0];
+    overlayNode.style.opacity = '';
+}
+
+
+icons.forEach(element => {
+    element.addEventListener('mouseover', iconOpacityChange);
+    element.addEventListener('mouseout', iconOpacityDefault);
+});
+
