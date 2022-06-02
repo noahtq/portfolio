4b57b987c14bbcb2c0c8c455a10590207dfb5609
noahtq <vfx.noaht@gmail.com>
2022-06-02 10:36:33 -0500

    Fix weather information and icon not displaying in browser

--- javascript/weather/updateWeather.js
+++ javascript/weather/updateWeather.js
@@ -1,11 +1,22 @@
 
 const weatherDiv = document.getElementsByClassName('weather-info')[0];
-const weatherIconNode = document.getElementsByClassName('weather-icon')[0].firstChild;
-const weatherConditionNode = document.getElementsByClassName('weather-condition')[0].firstChild;
-const weatherLocationNode = document.getElementsByClassName('weather-location')[0].firstChild;
-const weatherCurrentNode = document.getElementsByClassName('weather-current')[0].firstChild;
+const weatherIconNode = document.getElementsByClassName('weather-icon')[0].children[0];
+const weatherConditionNode = document.getElementsByClassName('weather-condition')[0].children[0];
+const weatherLocationNode = document.getElementsByClassName('weather-location')[0].children[0];
+const weatherCurrentNode = document.getElementsByClassName('weather-current')[0].children[0];
 const weatherHighNode = document.getElementsByClassName('weather-high')[0].children[1];
 const weatherLowNode = document.getElementsByClassName('weather-low')[0].children[1];
+
+
+function titleCase(string) {
+    const words = string.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        let newWord = words[i][0].toUpperCase();
+        newWord += words[i].slice(1);
+        words[i] = newWord;
+    }
+    return words.join(' ');
+}
 
 //location should be an array containing longitude and latitude information
 async function getCurrentWeatherData(localAPIKey, location) {
@@ -26,21 +37,56 @@ async function getCurrentWeatherData(localAPIKey, location) {
     }
 }
 
+function getWeatherIcon(id) {
+    const baseIconUrl = 'http://openweathermap.org/img/wn'
+    let icon = '';
+    const extension = '.png'
+    if (id >= 200 && id <= 232) {
+        icon = '11d';
+    } else if (id >= 300 && id <= 321) {
+        icon = '09d';
+    } else if (id >= 500 && id <= 504) {
+        icon = '10d';
+    } else if (id === 511) {
+        icon = '13d';
+    } else if (id >= 520 && id <= 531) {
+        icon = '09d';
+    } else if (id >= 600 && id <= 622) {
+        icon = '13d';
+    } else if (id >= 701 && id <= 781) {
+        icon = '50d';
+    } else if (id === 800) {
+        icon = '01d';
+    } else if (id === 801) {
+        icon = '02d';
+    } else if (id === 802) {
+        icon = '03d';
+    } else if (id === 803 || id === 804) {
+        icon = '04d';
+    }
+    return `${baseIconUrl}/${icon}${extension}`;
+};
+
 async function updateWeather(localAPIKey, location) {
     try{
         const jsonWeather = await getCurrentWeatherData(localAPIKey, location);
-        console.log(jsonWeather)
-        weatherConditionNode.innerHTML = jsonWeather.weather.description;
+
+        const conditionID = jsonWeather.weather[0].id;
+        const iconSrc = getWeatherIcon(conditionID);
+        weatherIconNode.src = iconSrc;
+
+        let weatherDescription = jsonWeather.weather[0].description;
+        weatherDescription = titleCase(weatherDescription);
+        weatherConditionNode.innerHTML = weatherDescription;
+
         weatherLocationNode.innerHTML = jsonWeather.name;
         weatherCurrentNode.innerHTML = Math.floor(jsonWeather.main.temp);
         weatherHighNode.innerHTML = Math.floor(jsonWeather.main.temp_max);
         weatherLowNode.innerHTML = Math.floor(jsonWeather.main.temp_min);
     } catch (error) {
         console.log(error);
-        weatherDiv.style.display = none;
+        weatherDiv.style.opacity = 0;
     }
 }
 
 updateWeather('bb4b1b99480f1a6b642420364d6d7427', [44.9778, -93.2650]);
-
-
